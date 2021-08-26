735c97e227e63a86664fb647429ff4ad641a49f7
CorreaPedro <pedro.ewerton29@gmail.com>
2021-08-25 22:39:26 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta();
+            minhaConta.Nome = "Pedro";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
